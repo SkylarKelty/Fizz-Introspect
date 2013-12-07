4a2e02226f910ae771857a2ee58020903581b436
Skylar Kelty <skylarkelty@gmail.com>
2013-12-07 13:15:03 +0000

    Fix test case

--- tests/SkylarK/Fizz/IntrospectorTest.php
+++ tests/SkylarK/Fizz/IntrospectorTest.php
@@ -6,12 +6,16 @@ class IntrospectorTest extends PHPUnit_Framework_TestCase
 		$this->introspector = new SkylarK\Fizz\Introspector("mysql:dbname=testdb;host=127.0.0.1", "travis", "");
 	}
 
-	public function test_TableCount() {
-		$this->assertEquals(1, $this->introspector->getTables());
+	public function test_Tables() {
+		$tables = $this->introspector->getTables();
+
+		$this->assertEquals(1, count($tables));
+		$this->assertEquals("Demo", $tables[0]);
 	}
 
 	public function test_ColumnMeta() {
 		$meta = $this->introspector->getMeta("Demo");
+
 		$this->assertEquals(2, count($meta));
 		$this->assertEquals("key", $meta[0]["name"]);
 		$this->assertEquals("value", $meta[1]["name"]);
